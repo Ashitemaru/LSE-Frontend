df6ef084199154202263d9afe854925b16846479
Ashitemaru <qhd19@mails.tsinghua.edu.cn>
2022-06-05 00:48:43 +0800

    Feature: Result page enables pagination

--- src/constants/captions.ts
+++ src/constants/captions.ts
@@ -1,6 +1,8 @@
 export const NULL_KEYWORD = "用于搜索的字符串不能为空";
 export const UNDEF_URL_PARAM_KEYWORD = "结果页面 URL 必须包含 keyword 字段";
 
+export const UNUNIFIED_TOTAL_RES = "网页缓存数据与平台不一致，请刷新网页重试";
+
 export const SEARCH_CAPTION_PRE = "搜索关键词【";
 export const SEARCH_CAPTION_MID1 = "】耗时 ";
 export const SEARCH_CAPTION_MID2 = " 秒，共计找到 ";
--- src/result/ResultScreen.tsx
+++ src/result/ResultScreen.tsx
@@ -2,8 +2,16 @@ import React, { useEffect, useState, useRef } from "react";
 import { useLocation, useNavigate } from "react-router-dom";
 import { BACKEND_URL_PREFIX } from "../constants/strings";
 import { uFetch } from "../util/network";
-import { message, Skeleton, Empty } from "antd";
-import { NULL_KEYWORD, SEARCH_CAPTION_MID1, SEARCH_CAPTION_MID2, SEARCH_CAPTION_PRE, SEARCH_CAPTION_SUF, UNDEF_URL_PARAM_KEYWORD } from "../constants/captions";
+import { message, Skeleton, Empty, Button, Pagination } from "antd";
+import {
+    NULL_KEYWORD,
+    SEARCH_CAPTION_MID1,
+    SEARCH_CAPTION_MID2,
+    SEARCH_CAPTION_PRE,
+    SEARCH_CAPTION_SUF,
+    UNDEF_URL_PARAM_KEYWORD,
+    UNUNIFIED_TOTAL_RES
+} from "../constants/captions";
 import { parseURLParam } from "../util/url";
 
 interface SearchResult {
@@ -17,7 +25,13 @@ const ResultScreen: React.FC = () => {
 
     const resultRef = useRef<SearchResult[]>([]);
     const [resultStamp, setStamp] = useState(new Date());
+
     const [refreshing, setRefreshing] = useState(true);
+    const [shouldForceRefresh, setForceRefresh] = useState(false);
+
+    const [total, setTotal] = useState(-1);
+    const [pageNum, setPageNum] = useState(0);
+    const [itemPerPage, setItemPerPage] = useState(10);
 
     // Parse the URL params
     const URLParam: Record<string, string> = parseURLParam(useLocation().search).result;
@@ -29,17 +43,56 @@ const ResultScreen: React.FC = () => {
         SEARCH_CAPTION_SUF
     );
 
+    const loadResult = () => {
+        setRefreshing(true);
+        uFetch(BACKEND_URL_PREFIX + "/demo/search", {
+            keyword: URLParam.keyword,
+            limit: "" + itemPerPage,
+            offset: "" + (pageNum * itemPerPage),
+        })
+            .then((res) => {
+                // Check or set the total number of results
+                // 'total === -1' means it is the first time to get the result
+                if (total === -1) {
+                    setTotal(res.count);
+                } else if (res.count != total) {
+                    message.error(UNUNIFIED_TOTAL_RES);
+                    setForceRefresh(true); // Force user to refresh the page
+                    setRefreshing(false);
+                    resultRef.current = [];
+                    return;
+                }
+
+                // eslint-disable-next-line @typescript-eslint/no-explicit-any
+                resultRef.current = res.hits.map((val: any) => ({
+                    id: parseInt(val.id as string, 10),
+                    title: val.id,
+                    content: val.content,
+                }));
+
+                setStamp(new Date());
+                setRefreshing(false);
+
+                console.log(`Search result updated at ${resultStamp}.`);
+                message.success(concatCaption(decodeURI(URLParam.keyword), res.time, res.count));
+            })
+            .catch((err) => {
+                message.error(err);
+                setRefreshing(false);
+            });
+    };
+
     /**
-     * @warn This is a VERY UGLY solution to deplicated call of useEffect.
+     * @warn This is a VERY UGLY solution to duplicated call of useEffect.
      * Remove it when a better solution is found.
      * Even useState cannot handle the problem.
      */
-    let called = false;
+    let initialized = false;
 
     useEffect(() => {
         // @warn: Remove it when a better solution is found.
-        if (called) return;
-        else called = true;
+        if (initialized) return;
+        else initialized = true;
 
         if (URLParam.keyword === undefined) {
             message.error(UNDEF_URL_PARAM_KEYWORD);
@@ -51,47 +104,58 @@ const ResultScreen: React.FC = () => {
             navigate("/");
         }
 
-        setRefreshing(true);
-        uFetch(BACKEND_URL_PREFIX + "/demo/search", {
-            keyword: URLParam.keyword,
-        })
-            .then((res) => {
-                // eslint-disable-next-line @typescript-eslint/no-explicit-any
-                resultRef.current = res.hits.map((val: any) => ({
-                    id: parseInt(val.id as string, 10),
-                    title: val.title,
-                    content: val.content,
-                }));
-                setStamp(new Date());
-                setRefreshing(false);
-
-                console.log(`Search result updated at ${resultStamp}.`);
-                message.success(concatCaption(
-                    decodeURI(URLParam.keyword), res.time, resultRef.current.length
-                ));
-            })
-            .catch((err) => {
-                message.error(err);
-                setRefreshing(false);
-            });
+        resultRef.current = [];
+        loadResult();
     }, []);
+
+    /**
+     * @warn This 'useEffect' must be placed behind the effect hook whose depList is [].
+     * This placement ensures that the 'initialized' variable is correctly set to 'true'
+     * and the this effect will not be executed.
+     * Remove it when a better solution is found.
+     */
+    useEffect(() => {
+        /**
+         * @note When initializing the page, the previous 'useEffect' has loaded the data
+         * & 'initialized' variable has been set to 'true', so we do not need to load data again here.
+         * When updating the page, the previous 'useEffect' will not be executed,
+         * & 'initialized' variable will be 'false' and we need to refresh data here.
+         */
+        if (!initialized) {
+            loadResult();
+        }
+    }, [pageNum, itemPerPage]);
 
     return (
         <div className="app-screen">
             {refreshing ? (
                 <Skeleton active />
             ) : (
-                resultRef.current.length ? (
+                shouldForceRefresh || resultRef.current.length === 0 ? (
+                    <Empty />
+                ) : (
                     <div style={{ flexDirection: "column", display: "flex" }}>
                         {resultRef.current.map((res) => (
-                            <div key={res.id}>
-                                {res.title}
-                            </div>
+                            <div key={res.id}>{res.title}</div>
                         ))}
-                    </div>
-                ) : (
-                    <div style={{ alignItems: "center", display: "flex" }}>
-                        <Empty />
+                        <Pagination
+                            total={total}
+                            showSizeChanger
+                            showQuickJumper
+                            showTotal={(o) => `Total ${o} results`}
+                            current={pageNum + 1}
+                            pageSize={itemPerPage}
+                            onChange={(page, pageSize) => {
+                                if (page - 1 != pageNum) {
+                                    setPageNum(page - 1);
+                                }
+
+                                if (pageSize != itemPerPage) {
+                                    setPageNum(0);
+                                    setItemPerPage(pageSize);
+                                }
+                            }}
+                        />
                     </div>
                 )
             )}
